8e9653330a77993ba25a57a83aea6be67b2029fb
BuHT1 <thisisbuht@gmail.com>
2019-07-28 14:58:56 +0300

    Add test with ArgumentCaptor

--- src/com/company/examplefortests/thirdexample/User.java
+++ src/com/company/examplefortests/thirdexample/User.java
@@ -1,5 +1,7 @@
 package com.company.examplefortests.thirdexample;
 
+import java.util.Objects;
+
 public class User {
     private Long id;
     private String firstName;
@@ -33,5 +35,20 @@ public class User {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
     }
 }
