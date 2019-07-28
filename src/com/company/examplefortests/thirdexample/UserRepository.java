3bd0c3da1a015cf54b6e661fd6745b6397296ef6
BuHT1 <thisisbuht@gmail.com>
2019-07-28 16:34:53 +0300

    Revert "Add test with ArgumentCaptor"
    This reverts commit 8e9653330a77993ba25a57a83aea6be67b2029fb.

--- src/com/company/examplefortests/thirdexample/UserRepository.java
+++ src/com/company/examplefortests/thirdexample/UserRepository.java
@@ -5,6 +5,5 @@ import java.util.ArrayList;
 public interface UserRepository {
     ArrayList<User> getAll();
     User findOneUser(Long id);
-    void save(User user);
     //More method's
 }
--- src/com/company/examplefortests/thirdexample/UserService.java
+++ src/com/company/examplefortests/thirdexample/UserService.java
@@ -6,23 +6,19 @@ public class UserService {
 
     UserRepository userRepository;
 
-    public UserService(UserRepository repository) {
-        userRepository = repository;
+    public UserService(UserRepository repository){
+        userRepository=repository;
     }
 
-    public ArrayList<User> getAll() {
+    public ArrayList<User> getAll(){
         return userRepository.getAll();
     }
 
-    public User findOneUser(Long id) {
+    public User findOneUser(Long id){
         return userRepository.findOneUser(id);
     }
 
-    public Integer getCountOfUsers() {
+    public Integer getCountOfUsers(){
         return userRepository.getAll().size();
-    }
-
-    public void saveUser(User user) {
-        userRepository.save(user);
     }
 }
